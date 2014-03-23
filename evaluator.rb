00234b0230ac0f79c0349e3e5dba040f94aca243
ChrisTimperley <christimperley@googlemail.com>
2014-03-23 14:29:00 +0000

    Finished implementing multi-threaded #evaluate! funtion.

--- evaluator.rb
+++ evaluator.rb
@@ -34,14 +34,19 @@ end
 # reached.
 #
 # ==== Parameters
+# [+random+]            A list of random number generators.
 # [+candidates+]        A list of candidates to evaluate.
 # [+opts+]              A hash of keyword options to this method.
 #
 # ==== Options
-# [+num_evaluations+]   The number of evaluations performed so far.
-# [+evaluation_limit+]  The maximum number of evaluations.
-# [+measure+]           The robustness measure being used.
-# [+benchmark+]         The benchmark function being used.
+# [+num_evaluations+]     The number of evaluations performed so far.
+# [+evaluation_limit+]    The maximum number of evaluations.
+# [+measure+]             The robustness measure being used.
+# [+benchmark+]           The benchmark function being used.
+# [+evaluation_threads+]  The number of threads available for evaluation.
+# [+grammar+]             The grammar being used by the evolution.
+# [+max_wraps+]           The maximum number of times the genome should
+#                         be wrapped during derivation.
 #
 # ==== Returns
 # The number of evaluations that were performed.
@@ -55,7 +60,26 @@ def evaluate!(candidates, num_evaluations, opts = {})
 		candidates = candidates.shuffle.[0...evals_remaining]
 	end
 
+  # Extract the samples and variable set for the benchmark.
+  samples = opts[:samples][opts[:benchmark]]
+  variables = samples[0].length == 3 ? ['x', 'y'] : ['x']
 
+  # Split the list of candidates across each of the available threads.
+  # If there is an error during the evaluation of a candidate then
+  # set the fitness of that candidate to infinity (the worst possible).
+  candidates.peach do |c|
+    begin
+      lm = compile(c.chromosome,
+        variables: variables,
+        grammar: opts[:grammar],
+        measure: opts[:measure])
+      c.fitness = samples.reduce(0) do |sum, sample|
+        sum += lm[*sample[0...-1]] - sample[-1])**2
+      end
+    rescue StandardError
+      c.fitness = Infinity
+    end
+  end
 
   # Return the number of evaluated candidates.
   return candidates.length
