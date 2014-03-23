1fe29bd23ff5e1290fbda868f7eec59a5010fa26
ChrisTimperley <christimperley@googlemail.com>
2014-03-23 16:11:40 +0000

    Added function set.

---
+++ functions.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+# 
+# Define the function sets.
+
+module FunC
+	def self.add(x, y)
+		x+y
+	end
+	def self.sub(x, y)
+		x-y
+	end
+	def self.mul(x, y)
+		x*y
+	end
+	def self.div(x, y)
+		x/y
+	end
+end
