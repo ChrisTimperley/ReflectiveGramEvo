041457949a213c1ec58794db16e46cff011adb53
ChrisTimperley <christimperley@googlemail.com>
2014-03-23 17:07:43 +0000

    Corrected typos in #evaluate! and #compile

--- evaluator.rb
+++ evaluator.rb
@@ -9,6 +9,8 @@
 # ==== Parameters
 # [+sequence+]  The integer sequence to compile.
 # [+grammar+]   The grammar to use to create the derivation.
+#
+# ==== Options
 # [+variables+] The variables of the compiled function.
 # [+measure+]   The name of the robustness measure being used.
 # [+max_wraps+] The maximum number of times the genome should be wrapped
@@ -17,15 +19,15 @@
 # ==== Returns
 # A callable lambda function of a type appropriate the robustness
 # measures being used.
-def compile(sequence, grammar, variables, measure, max_wraps = 1)
-  derivation = grammar.derive(sequence, max_wraps: max_wraps)
-  case measure
+def compile(sequence, grammar, opts = {})
+  derivation = grammar.derive(sequence, max_wraps: opts[:max_wraps])
+  case opts[:measure]
   when 'Global'
-    return ToRobust::Global::GlobalRobustLambda(variables, derivation)
+    return ToRobust::Global::GlobalRobustLambda(opts[:variables], derivation)
   when 'Local'
-    return ToRobust::Local::LocalRobustLambda(variables, derivation, [FunC])
+    return ToRobust::Local::LocalRobustLambda(opts[:variables], derivation, [FunC])
   else
-    return derivation.to_lambda(variables)
+    return derivation.to_lambda(opts[:variables])
   end
 end
 
@@ -56,8 +58,8 @@ def evaluate!(candidates, opts = {})
   # If there are more candidate solutions than there are remaining
   # evaluations, then shuffle the list of candidates and evaluate
   # as many as possible.
-	evals_remaining = opts[:evaluation_limit] - num_evaluations
-	if evals_remaining > candidates
+	evals_remaining = opts[:evaluation_limit] - opts[:num_evaluations]
+	if evals_remaining > candidates.length
 		candidates = candidates.shuffle[0...evals_remaining]
 	end
 
@@ -68,17 +70,17 @@ def evaluate!(candidates, opts = {})
   # Split the list of candidates across each of the available threads.
   # If there is an error during the evaluation of a candidate then
   # set the fitness of that candidate to infinity (the worst possible).
-  candidates.peach do |c|
+  candidates.peach(opts[:evaluation_threads]) do |c|
     begin
       lm = compile(c.chromosome,
+        opts[:grammar],
         variables: variables,
-        grammar: opts[:grammar],
         measure: opts[:measure])
       c.fitness = samples.reduce(0) do |sum, sample|
         sum += (lm[*sample[0...-1]] - sample[-1])**2
       end
     rescue StandardError
-      c.fitness = Float::Infinity
+      c.fitness = Float::INFINITY
     end
   end
 
