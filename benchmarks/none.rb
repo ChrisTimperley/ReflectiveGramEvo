e00f48f54cb44d45adf0ca51ec4d386cdd5f7c9b
ChrisTimperley <christimperley@googlemail.com>
2014-03-31 09:54:10 +0100

    Added setup for benchmark experiments using no robustness measures.

---
+++ benchmarks/none.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+require_relative 'base'
+
+run('None',
+  'C:\Users\Steven\Documents\Code\ReflectiveGramEvo\benchmarks\none', {
+  'Population' => 189,
+  'Elites' => 0.438,
+  'Tournament' => 2,
+  'Mutation' => 0.375,
+  'Crossover' => 0.764
+})
