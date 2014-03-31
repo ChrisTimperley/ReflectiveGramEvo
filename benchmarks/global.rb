f617cd30c399fc8080f0b276c68a9801f88a602e
ChrisTimperley <christimperley@googlemail.com>
2014-03-31 09:55:29 +0100

    Added setup for benchmark experiments using global robustness measures.

---
+++ benchmarks/global.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+require_relative 'base'
+
+run('None',
+  'C:\Users\Steven\Documents\Code\ReflectiveGramEvo\benchmarks\global', {
+  'Population' => 161,
+  'Elites' => 0.440,
+  'Tournament' => 2,
+  'Mutation' => 0.425,
+  'Crossover' => 0.705
+})
